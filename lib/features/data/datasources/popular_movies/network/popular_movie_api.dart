03ec7b39212a5bc6d4119350ad04c5ae71d78a68
rezkyauliapratama <rezkyauliapratama@gmail.com>
2020-07-27 17:22:33 +0700

    add get_it dep and popular_movie_api

--- lib/features/data/datasources/popular_movies/network/popular_movie_api.dart
+++ lib/features/data/datasources/popular_movies/network/popular_movie_api.dart
@@ -1,15 +1,26 @@
+import 'package:box_office_clean_arch/core/error/exceptions.dart';
+import 'package:box_office_clean_arch/features/data/datasources/common/secret_components.dart';
+import 'package:box_office_clean_arch/features/data/models/ListMovieModel.dart';
+import 'package:dio/dio.dart';
 
-//
-//class MoviesProviderApi implements PopularMovieDataSource {
-//  final Dio _dio;
-////  final SecretLoader _secretLoader;
-//  final String _urlPopularMovies = "/movie/popular";
-//
-//  MoviesProviderApi(this._dio, this._secretLoader);
-//
-//
-//  @override
-//  Future<ListMovieModel> getPopularMovies() async {
-//  }
-//
-//}}
+import '../popular_movie_datasource.dart';
+
+class MoviesProviderApi implements PopularMovieDataSource {
+  final Dio _dio;
+  final Secret _secret;
+  final String _urlPopularMovies = "/movie/popular";
+
+  MoviesProviderApi(this._dio, this._secret);
+
+  @override
+  Future<ListMovieModel> getPopularMovies() async {
+    try {
+      Response response = await _dio.get(_urlPopularMovies,
+          queryParameters: {"api_key": _secret.apiKey, "language": "en-US"});
+      return ListMovieModel.fromJson(response.data);
+    } catch (error, stacktrace) {
+      print("Exception occured: $error stackTrace: $stacktrace");
+      throw ServerException();
+    }
+  }
+}
